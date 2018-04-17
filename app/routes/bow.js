025a8eff1b3f5c296606565657098339d467d10c
andrewinnocent <innocent.andrew@gmail.com>
2018-04-17 07:55:47 -0400

    Adds Quantity Component
    Action not completed. Need to decide if action will only be used in Cart
    or both Cart and Bow routes.

--- app/routes/bow.js
+++ app/routes/bow.js
@@ -4,5 +4,10 @@ export default Route.extend({
   model (params) {
     console.log('params is', params)
     return this.get('store').findRecord('bow', params.bow_id)
+  },
+  actions: {
+    changeQuantity (qty) {
+      // should this be handled in the Cart? Easier?
+    }
   }
 })
