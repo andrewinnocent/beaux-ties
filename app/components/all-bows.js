0859e58f72e01fa9d0a8d31650b59aa15d5c6170
andrewinnocent <innocent.andrew@gmail.com>
2018-04-18 09:38:46 -0400

    Adds All-Bows Component
    Needed to create a component to be able to use the `auth` &
    `isAuthenticated` service. Shopping cart now renders when signed in.

---
+++ app/components/all-bows.js
@@ -0,0 +1,9 @@
+import { alias } from '@ember/object/computed'
+import Component from '@ember/component'
+import { inject as service } from '@ember/service'
+
+export default Component.extend({
+  auth: service(),
+  user: alias('auth.credentials.email'),
+  isAuthenticated: alias('auth.isAuthenticated')
+})
--- app/routes/bows.js
+++ app/routes/bows.js
@@ -1,9 +1,7 @@
-import { alias } from '@ember/object/computed'
 import Route from '@ember/routing/route'
 
 export default Route.extend({
   model () {
     return this.get('store').findAll('bow')
-  },
-  isAuthenticated: alias('auth.isAuthenticated')
+  }
 })
