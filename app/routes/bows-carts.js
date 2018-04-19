816a422a6e8ec3028c611846f97821dcaff0ca7b
andrewinnocent <innocent.andrew@gmail.com>
2018-04-19 19:59:13 -0400

    Adds Ember Toast
    Added notification for checkout button.

--- app/routes/bows-carts.js
+++ app/routes/bows-carts.js
@@ -1,16 +1,20 @@
 import Route from '@ember/routing/route'
+import { inject as service } from '@ember/service'
 
 export default Route.extend({
   model () {
     return this.get('store').findAll('bows-cart')
   },
+  notifications: service('toast'),
   actions: {
     removeFromCart (removeBow) {
       removeBow.destroyRecord()
     //     .then(() => {this.transitionTo('bows'))
     },
     checkout () {
-      console.log('checkout clicked')
+      // console.log('checkout clicked')
+      const notifications = this.get('notifications')
+      notifications.info('Sorry for the incovenience! Checkout is not available, yet.', 'That\'s awkward...', {positionClass: 'toast-bottom-right'})
     }
   }
 })
