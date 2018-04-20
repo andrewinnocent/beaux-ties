026543f74832a67825c5b947115a83fdf6217554
andrewinnocent <innocent.andrew@gmail.com>
2018-04-19 21:14:05 -0400

    Adds Messages
    Add to cart, remove from cart, add to wishlist.

--- app/routes/bow.js
+++ app/routes/bow.js
@@ -1,13 +1,27 @@
 import Route from '@ember/routing/route'
+import { inject as service } from '@ember/service'
 
 export default Route.extend({
   model (params) {
     return this.get('store').findRecord('bow', params.bow_id)
   },
+  notifications: service('toast'),
   actions: {
     addToCart (addBow) {
       const addNewBow = this.get('store').createRecord('bows-cart', { bow: addBow })
       addNewBow.save()
+      const notifications = this.get('notifications')
+      notifications.success('Your Beaux-Tie\'s been added!', 'Added to Cart', {positionClass: 'toast-bottom-right'})
     }
+    // addToWish (bowToAdd) {
+    //   console.log('bowToAdd is', bowToAdd)
+    //   const newWish = this.get('store').createRecord('wish', {
+    //     bow: bowToAdd,
+    //     active: true
+    //   })
+    //   newWish.save()
+    //   const notifications = this.get('notifications')
+    //   notifications.success('Your Beaux-Tie\'s been added!', 'Added to Wishlist', {positionClass: 'toast-bottom-right'})
+    // }
   }
 })
--- app/routes/bows.js
+++ app/routes/bows.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route'
 import RSVP from 'rsvp'
+import { inject as service } from '@ember/service'
 
 export default Route.extend({
   model () {
@@ -8,11 +9,13 @@ export default Route.extend({
       wishes: this.get('store').findAll('wish')
     })
   },
-
+  notifications: service('toast'),
   actions: {
     addToCart (addBow) {
       const addNewBow = this.get('store').createRecord('bows-cart', { bow: addBow })
       addNewBow.save()
+      const notifications = this.get('notifications')
+      notifications.success('Your Beaux-Tie\'s been added!', 'Added to Cart', {positionClass: 'toast-bottom-right'})
     },
     addToWish (bowToAdd) {
       // console.log('bowToAdd is', bowToAdd)
@@ -21,6 +24,8 @@ export default Route.extend({
         active: true
       })
       newWish.save()
+      const notifications = this.get('notifications')
+      notifications.success('Your Beaux-Tie\'s been added!', 'Added to Wishlist', {positionClass: 'toast-bottom-right'})
     }
   }
 })
