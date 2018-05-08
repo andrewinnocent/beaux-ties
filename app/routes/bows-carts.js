faa76132d7beeacfe7a17f1d3b33e239cab1b8f4
andrewinnocent <innocent.andrew@gmail.com>
2018-05-08 11:59:39 -0400

    Adds Decimals to Total
    Installed string-helper addon to manipulate number formats. Removed
    unnecessary console log in route.

--- app/routes/bows-carts.js
+++ app/routes/bows-carts.js
@@ -3,7 +3,6 @@ import { inject as service } from '@ember/service'
 
 export default Route.extend({
   model () {
-    // console.log(this.get('store').findAll('bows-cart'))
     return this.get('store').findAll('bows-cart')
   },
   notifications: service('toast'),
@@ -15,7 +14,6 @@ export default Route.extend({
       notifications.success('Go ahead and browse more Beaux-Ties!', 'Beaux-Tie Removed From Cart', {positionClass: 'toast-bottom-right'})
     },
     checkout () {
-      // console.log('checkout clicked')
       const notifications = this.get('notifications')
       notifications.info('Checkout is not actually available.', 'It\'s a demo!', {positionClass: 'toast-bottom-right'})
     }
